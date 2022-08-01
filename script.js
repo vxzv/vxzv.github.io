df28afa172dd7da4a0e481d6d1f1d7592438bad0
vxzv <110003684+vxzv@users.noreply.github.com>
2022-08-01 11:11:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -120,3 +120,19 @@ $.scrollify({
 	$(function() {
     	$('html,body').animate({ scrollTop: 0 }, '1');
 	});
+
+//scroll_effect
+$(window).scroll(function () {
+	var scrollAnimationElm = document.querySelectorAll('.fade-in');
+	var scrollAnimationFunc = function () {
+	  for (var i = 0; i < scrollAnimationElm.length; i++) {
+		var triggerMargin = 100;
+		if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+		  scrollAnimationElm[i].classList.add('on');
+		}
+	  }
+	}
+	window.addEventListener('load', scrollAnimationFunc);
+	window.addEventListener('scroll', scrollAnimationFunc);
+  });
+	
