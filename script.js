f121299a7f8a9d14f4abee8a9ec5a4727067577c
vxzv <110003684+vxzv@users.noreply.github.com>
2022-08-24 13:57:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 $(window).on('load',function(){
     $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
 
@@ -159,4 +160,57 @@ $(window).scroll(function () {
 	window.addEventListener('load', scrollAnimationFunc);
 	window.addEventListener('scroll', scrollAnimationFunc);
   });
-	
+
+  		//aboutusとfeatureの高さを取得してparticles.jsのcssに代入したい。
+		  $(document).ready(function areaheight() {
+			const areaheight = $("#area2").outerHeight(true) + $("#area3").outerHeight(true) + $("#cardlayout-wrap").outerHeight(true);
+			$(".container2").css("height", areaheight)+ 'px';
+	  
+			});
+
+		//高さ取得がウィンドウサイズ変えると崩れるのでリロード
+				var timer = 0;
+				var currentWidth = window.innerWidth;
+				$(window).resize(function(){
+					if (currentWidth == window.innerWidth) {
+						return;
+					}
+					if (timer > 0) {
+						clearTimeout(timer);
+					}
+			 
+					timer = setTimeout(function () {
+						location.reload();
+					}, 200);
+					
+				});	
+
+		//同じ日付で2回目以降ならローディング画面非表示の設定
+
+var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
+var myD = new Date();//日付データを取得
+var myYear = String(myD.getFullYear());//年
+var myMonth = String(myD.getMonth() + 1);//月
+var myDate = String(myD.getDate());//日
+    
+if (splash_text != myYear + myMonth + myDate) {//cookieデータとアクセスした日付を比較↓
+        $("#splash").css("display", "block");//１回目はローディングを表示
+        setTimeout(function () {
+            $("#splash_logo").fadeIn(1000, function () {//1000ミリ秒（1秒）かけてロゴがフェードイン
+                setTimeout(function () {
+            $("#splash_logo").fadeOut(1000);//1000ミリ秒（1秒）かけてロゴがフェードアウト
+                }, 1000);//1000ミリ秒（1秒）後に処理を実行
+        setTimeout(function () {
+            $("#splash").fadeOut(1000, function () {//1000ミリ秒（1秒）かけて画面がフェードアウト
+            var myD = new Date();
+            var myYear = String(myD.getFullYear());
+            var myMonth = String(myD.getMonth() + 1);
+            var myDate = String(myD.getDate());
+            $.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
+        });
+        }, 1700);//1700ミリ秒（1.7秒）後に処理を実行
+    });
+}, 1000);//1000ミリ秒（1秒）後に処理を実行
+}else {
+    $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
+}  
